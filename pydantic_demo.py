af0c022cb59457ed0adfea8bda174051aaa59e0b
aafaq <maafaq.mscs23seecs@seecs.edu.pk>
2025-06-19 19:12:52 +0500

    learned about EmailStr,Field and attributes of field to control the extra checks

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -1,11 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr,Field
+from typing import Optional
 
 class Student(BaseModel):
     
     name:str = "aafaq"
+    age:Optional[int]
+    email:EmailStr
+    cgpa:float=Field(gt=0,lt=10,default=5,description="a floating point number to represent grade of a student ")
     
     
-new_student={}  # it will throw an error if you dont follow u the define format 
+new_student={'age':32,'email':"abc@gmail.com"}  # it will throw an error if you dont follow u the define format 
 student=Student(**new_student)
 
 print(student)
